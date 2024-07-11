23e8a10643911459ea75ead181e3bdfb6c5b787d
luxuereal <luxuereal@gmail.com>
2024-07-10 22:44:08 -0400

    Fix/router-issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-"use client"
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/coinflip");
-  }, []);
 
   return <main className="flex"></main>;
 }
