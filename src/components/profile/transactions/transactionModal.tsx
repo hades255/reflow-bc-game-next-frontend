65cee86297e0d45ddd2a045e942a1f7979c3b801
maksym418 <musiichukmaksym418@gmail.com>
2024-07-15 19:49:19 -0400

    profiles & signout & persist redux

---
+++ src/components/profile/transactions/transactionModal.tsx
@@ -0,0 +1,122 @@
+import React, { useCallback } from "react";
+import moment from "moment";
+import IconCoin from "@/utils/icons/Coin";
+import RoyalflipCoin from "@/utils/icons/Royalfilp";
+
+interface Transaction {
+  id: number;
+  type: string;
+  game_id: number;
+  amount: number;
+  status: string;
+  created_at: string;
+}
+
+interface Props {
+  transaction: Transaction;
+  setSelected: Function;
+}
+
+const TransactionModal: React.FC<Props> = ({ transaction, setSelected }) => {
+  const getDateFormat = useCallback(() => {
+    const currentYear = new Date().getFullYear();
+    const year = moment(transaction.created_at).year();
+    if (currentYear === Number(year)) {
+      return moment(transaction.created_at).format("DD MMM HH:mm");
+    }
+    return moment(transaction.created_at).format("DD MMM, YYYY HH:mm");
+  }, [transaction]);
+
+  const handleClickClose = useCallback(() => {
+    setSelected(null);
+  }, [setSelected]);
+
+  return (
+    <div
+      className="relative z-50"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        className="fixed inset-0 bg-gray-900 bg-opacity-70 transition-opacity"
+        aria-hidden="true"
+      ></div>
+
+      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg text-[#D8D8E8] bg-[#25252E] text-left shadow-xl transition-all w-[360px]">
+            <div className="w-full flex justify-end pt-4 pr-4">
+              <span>X</span>
+            </div>
+            <div className="w-full flex justify-center font-bold p-2 pt-0">
+              {transaction.type.substring(0, 1).toUpperCase() +
+                transaction.type.substring(1)}
+            </div>
+            <div className="w-full flex justify-center">
+              Placed on {getDateFormat()}
+            </div>
+            <div className="w-full p-2 px-8 pb-0">
+              <div className="w-full rounded-t-lg bg-[#20202A] p-4">
+                <div className="flex justify-between align-middle">
+                  <div className="flex">
+                    <div>
+                      <RoyalflipCoin height={60} width={66} color="white" />
+                    </div>
+                    <div className="flex flex-col">
+                      <div>Round #9516594</div>
+                      <div>CT</div>
+                    </div>
+                  </div>
+                  <div className="pt-4">Verify</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full p-2 px-8 pt-1">
+              <div className="w-full rounded-b-lg bg-[#20202A] p-4">
+                <div className="flex justify-between">
+                  <div>Bet Placed on</div>
+                  <div className="flex">
+                    <span className="pt-[2px]">
+                      <RoyalflipCoin height={20} width={22} color="white" />
+                    </span>
+                    <div>CT</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full p-2 px-8">
+              <div className="w-full rounded-lg bg-[#20202A] p-4 flex justify-between">
+                <span>{transaction.amount > 0 ? "You Won" : "You Lose"}</span>
+                <div className="flex">
+                  <span className="pt-[1px] pr-[2px]">
+                    <IconCoin width={14} height={14} color="#E9AE15" />
+                  </span>
+                  <span
+                    className={`${
+                      transaction.amount > 0 ? "text-green-600" : "text-red-600	"
+                    } font-medium text-[12px]`}
+                  >
+                    {transaction.amount}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="w-full p-2 px-8 mb-6">
+              <button
+                className="bg-[#333541] py-2 px-4 rounded-sm w-full"
+                onClick={handleClickClose}
+              >
+                <div className="flex justify-center items-center">
+                  <p className="font-semibold text-xs">Close</p>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TransactionModal;
--- src/redux/slices/main/userSlice.tsx
+++ src/redux/slices/main/userSlice.tsx
@@ -6,7 +6,7 @@ import { UserType } from "@/utils/types";
 const user: UserType | null = null;
 
 interface StateType {
-  user: UserType | null
+  user: UserType | null;
 }
 
 const initialState: StateType = {
@@ -17,19 +17,19 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<UserType>) => {
+    setUser: (state, action: PayloadAction<UserType | null>) => {
       state.user = action.payload;
     },
     deleteUser: (state) => {
       state.user = null;
     },
     updateBalance: (state, action: PayloadAction<{ balance: number }>) => {
-      state.user = state.user && ({
+      state.user = state.user && {
         ...state.user,
-        prev_balance: Number({...state.user}.balance),
-        balance: Number(state.user.balance) + action.payload.balance
-      })
-    }
+        prev_balance: Number({ ...state.user }.balance),
+        balance: Number(state.user.balance) + action.payload.balance,
+      };
+    },
   },
 });
 
@@ -37,6 +37,7 @@ export const { setUser, deleteUser, updateBalance } = userSlice.actions;
 
 export const useUser = () => useSelector((state: RootState) => state.user.user);
 
-export const useBalance = () => useSelector((state: RootState) => state.user.user?.balance);
+export const useBalance = () =>
+  useSelector((state: RootState) => state.user.user?.balance);
 
 export default userSlice.reducer;
