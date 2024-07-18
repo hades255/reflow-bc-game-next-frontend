65f1ad30a396ec0f82aa19f0480342487d596e93
luxuereal <luxuereal@gmail.com>
2024-07-17 21:00:18 -0400

    Fix / better-table

--- src/components/roulette/BetterTable.tsx
+++ src/components/roulette/BetterTable.tsx
@@ -29,7 +29,7 @@ const BetterTable: FC<Props> = ({ type, betters }) => {
           />
           {"Place Bet"}
         </div>
-        <span>{type === 3 ? "14" : "2"}x</span>
+        <span>Win {type === 3 ? "14" : "2"}x</span>
       </div>
       <div className="w-full rounded-md bg-[#1E1E1E] game-card p-3 flex justify-between items-center relative">
         <div className="h-full w-full rounded-md innerBlack bg-[#191919]">
