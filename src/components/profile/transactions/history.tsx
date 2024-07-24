4bcba008ab59e3ac86444c7c51a4ab5ac762c882
maksym418 <musiichukmaksym418@gmail.com>
2024-07-23 22:16:37 -0400

    fix issues on profile/transactions page

--- src/components/profile/transactions/history.tsx
+++ src/components/profile/transactions/history.tsx
@@ -102,8 +102,7 @@ const HistoryTab: FC<HistoryTabProps> = ({
         </span>
       </div>
       <div className="w-[100px] flex-none text-[#99A]">
-        {/* {transaction.game_id} */}
-        #9516594
+        {transaction.game_id}
       </div>
       <div className="flex-grow"></div>
       <div className="flex flex-row items-center gap-1 w-[100px]">
