00512bd72c9bb5f89adb1796beb3696732fbe04f
maksym418 <musiichukmaksym418@gmail.com>
2024-09-14 22:33:54 -0400

    profit lose chart

--- src/components/profile/details/LabelItem.tsx
+++ src/components/profile/details/LabelItem.tsx
@@ -10,17 +10,17 @@ const LabelItem: FC<Props> = ({ text = "All", active, onClick }) => {
   return (
     <div className="p-[6px] cursor-pointer" onClick={onClick}>
       <p
-        className={`text-[10px] font-semibold  ${
-          active ? "text-[#E9AE15]" : "text-[#838383]"
-        }`}
+        className={`text-[10px] font-semibold`}
+        style={{ color: active ? "#E9AE15" : "#838383" }}
       >
         {text}
       </p>
       {active && (
         <div
-          className="h-[0.5px] w-auto mt-[2px]"
+          className="h-[1px] w-auto mt-[2px]"
           style={{
-            background: "linear-gradient(to left, #1e1e1e, #E9AE15,#1e1e1e)",
+            background:
+              "linear-gradient(to left, #12121255, #E9AE1555,#12121255)",
           }}
         ></div>
       )}
