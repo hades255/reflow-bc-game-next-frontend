a2341854625616e3c9a01c22a0840fbbbf06030c
bberry124 <renning100@gmail.com>
2024-10-04 05:02:25 +0900

    update arrowIcon

--- src/utils/icons/UpDownArrow.tsx
+++ src/utils/icons/UpDownArrow.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const UpDownArrow = () => {
   return (
     <svg
-      width="5"
-      height="6"
+      width="9"
+      height="10"
       viewBox="0 0 5 6"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
