6280139b53a94a76be256f25f89b88939b1415a7
maksym418 <musiichukmaksym418@gmail.com>
2024-10-07 04:25:37 -0500

    fix issues on footer

--- src/app/roulette/page.tsx
+++ src/app/roulette/page.tsx
@@ -2,7 +2,7 @@ import RoulettePage from "@/components/roulette/RoulettePage"
 
 const Roulette = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 min-h-[calc(100vh_-_130px)]">
       <RoulettePage />
     </div>
   )
