a029880f40be3dafddd621fdc4f258a9e5de3538
maksym418 <musiichukmaksym418@gmail.com>
2024-10-07 12:35:36 -0500

    update footer

--- src/components/layouts/main-layout/AppFooter.tsx
+++ src/components/layouts/main-layout/AppFooter.tsx
@@ -1,22 +1,22 @@
 "use client";
 import React, { FC } from "react";
 import Image from "next/image";
-import logo from "@/assets/logos/logo.png";
 import {
   DiscordImg,
   SupportImg,
   TwitchImg,
   TwitterImg,
 } from "@/assets/icons/footer/Footer";
+import logo from "@/assets/logos/logo.png";
 import ukIcon from "@/assets/logos/flags/uk.png";
 
 const AppFooter: FC = () => {
   return (
-    <footer className="w-full p-12 text-[#5D5D5D]">
-      <div className="p-6 flex justify-between min-h-max mobile:flex-col mobile:gap-[20px] mobile:px-[20px] mobile:py-[30px] mobile:mb-[26px] rounded-lg innerBlack">
-        <div className="flex justify-between md:gap-[57px] gap-[30px] mobile:flex-col mobile:gap-[15px] mobile:justify-start mobile:items-center">
-          <div className="flex flex-col gap-[12px] max-w-[300px] mobile:max-w-full mobile:w-full mobile:items-center items-start">
-            <div className="mobile:!p-[0px]">
+    <footer className="w-full p-12 pb-4 text-[#5D5D5D]">
+      <div className="px-6 pt-10 pb-4 flex justify-between min-h-max max-[1000px]:flex-col max-[1000px]:gap-[20px] max-[1000px]:px-[20px] max-[1000px]:py-[30px] max-[1000px]:mb-[26px] rounded-lg innerBlack">
+        <div className="flex justify-between md:gap-[57px] gap-[30px] max-[1000px]:flex-col max-[1000px]:gap-[15px] max-[1000px]:justify-start max-[1000px]:items-center">
+          <div className="flex flex-col gap-[12px] max-w-[340px] max-[1000px]:max-w-full max-[1000px]:w-full max-[1000px]:items-center items-start">
+            <div className="max-[1000px]:!p-[0px]">
               <Image
                 src={logo}
                 alt="logo"
@@ -27,11 +27,11 @@ const AppFooter: FC = () => {
               />
             </div>
             <span className="text-lg">© 2024 REFLOW </span>
-            <p className="text-xs mobile:text-center leading-[14px] text-md">
+            <p className="text-xs max-[1000px]:text-center leading-[14px] text-md">
               REFLOW is owned and operated by RUNITUP LTD located at Avlonos, 1
               Maria house 1075, Nicosia, Cyprus.
             </p>
-            <div className="mobile:hidden flex items-center gap-[10px]">
+            <div className="max-[1000px]:hidden flex items-center gap-[10px]">
               <div>
                 <div className="w-9 h-9 rounded-[18px] bg-gray-600 flex justify-center items-center text-white">
                   18+
@@ -42,35 +42,9 @@ const AppFooter: FC = () => {
                 older.
               </span>
             </div>
-            <div className="hidden flex-col gap-[20px] max-w-[320px] mobile:w-full">
-              <div className="max-w-[520px] text-xs leading-[18px] text-md mobile:w-full mobile:text-center">
-                At Reflow, we{"’"}re committed to providing the fairest and most
-                thrilling gaming experience, featuring the industry{"’"}s best
-                house edge. Join us today and play with confidence, knowing that
-                luck is on your side.
-              </div>
-              <a
-                href="mailto:support@reflow.im"
-                target="_blank"
-                className="h-fit w-fit mb-[15px] mt-[7px] select-none mobile:hidden"
-              >
-                <div className="text-white text-lg">support@reflow.im</div>
-              </a>
-              <a
-                className="group/support w-fit cursor-pointer mobile:hidden"
-                rel="noopener noreferrer"
-              >
-                <div className="w-[160px] flex justify-center items-center text-xl gap-[8px] p-[10px] bg-raisin-black rounded-[8px] text-white">
-                  <SupportImg color="white" />
-                  <span className="footer_support__gb0LW group-hover/support:text-cool-grey transition duration-300">
-                    Support
-                  </span>
-                </div>
-              </a>
-            </div>
           </div>
-          <div className="flex flex-col gap-[20px] max-w-[320px] mobile:w-full">
-            <div className="max-w-[520px] text-xs leading-[18px] text-md mobile:w-full mobile:text-center">
+          <div className="flex flex-col gap-[20px] max-w-[360px] max-[1000px]:w-full">
+            <div className="max-w-[520px] text-xs leading-[18px] text-md max-[1000px]:w-full max-[1000px]:text-center">
               At Reflow, we{"’"}re committed to providing the fairest and most
               thrilling gaming experience, featuring the industry{"’"}s best
               house edge. Join us today and play with confidence, knowing that
@@ -79,15 +53,15 @@ const AppFooter: FC = () => {
             <a
               href="mailto:support@reflow.im"
               target="_blank"
-              className="h-fit w-fit mb-[15px] mt-[7px] select-none mobile:hidden"
+              className="h-fit w-fit mb-[15px] mt-[7px] select-none max-[1000px]:hidden"
             >
               <div className="text-white text-lg">support@reflow.im</div>
             </a>
             <div
-              className="group/support w-fit cursor-pointer mobile:hidden"
+              className="group/support w-fit cursor-pointer max-[1000px]:hidden"
               rel="noopener noreferrer"
             >
-              <div className="w-[160px] flex justify-center items-center text-xl gap-[8px] p-[10px] bg-raisin-black rounded-[8px] text-white">
+              <div className="w-[160px] flex justify-center items-center text-xl gap-[8px] p-[10px] bg-raisin-black rounded-[8px] text-white border border-[#5D5D5D]">
                 <SupportImg color="white" />
                 <span className="footer_support__gb0LW group-hover/support:text-cool-grey transition duration-300">
                   Support
@@ -96,8 +70,8 @@ const AppFooter: FC = () => {
             </div>
           </div>
         </div>
-        <div className="flex justify-between md:gap-[90px] gap-[30px] ml-[10px] mobile:flex-wrap mobile:gap-[20px]">
-          <div className="flex flex-col gap-[10px] mobile:w-[calc(50%_-_25px)]">
+        <div className="flex justify-between gap-[30px] ml-[10px] max-[1000px]:flex-wrap max-[1000px]:gap-[20px]">
+          <div className="flex flex-col gap-[6px] min-w-[150px] max-[1000px]:w-[calc(50%_-_25px)]">
             <div className="text-white text-sm">About:</div>
             <span className="text-sm select-none transition-all overflow-hidden overflow-ellipsis capitalize cursor-pointer hover:text-white">
               Fairness
@@ -112,24 +86,22 @@ const AppFooter: FC = () => {
               AML Policy
             </span>
           </div>
-          <div className="flex flex-col gap-[10px] mobile:w-[_40%] mobile:items-start">
+          <div className="flex flex-col gap-[10px] max-[1000px]:w-[_40%] max-[1000px]:items-start">
             <div className="text-white text-xs text-center">Community</div>
             <div className="select-none flex justify-center gap-[10px] items-center">
-              <div className="flex mobile:flex-col items-center gap-[10px]">
+              <div className="flex max-[1000px]:flex-col items-center gap-[10px]">
                 <a href="https://discord.gg/reflow" target="_blank">
                   <DiscordImg color={"#767B86"} />
                 </a>
                 <a href="https://www.twitch.tv/reflow" target="_blank">
                   <TwitchImg color={"#787c84"} />
                 </a>
-              </div>
-              <div className="flex flex-col">
                 <a href="https://twitter.com/reflow" target="_blank">
                   <TwitterImg color={"#767B86"} />
                 </a>
               </div>
             </div>
-            <div className="footer_languageSwitcher__rUqv_ flex mobile:hidden flex-col gap-[0px] w-full h-[80px]">
+            <div className="flex flex-col gap-[0px] w-full h-[80px]">
               <div className="language_short__bH9Qg !justify-start !items-start group/active hover: cursor-pointer w-full h-full flex transition duration-300 text-xl gap-[5px] text-white relative">
                 <div className="flex items-center gap-[10px]">
                   <Image
@@ -140,70 +112,27 @@ const AppFooter: FC = () => {
                   <span className="text-white select-none text-sm">ENG</span>
                 </div>
               </div>
-              <div
-                className="group/support w-fit cursor-pointer hidden mobile:flex"
-                rel="noopener noreferrer"
-              >
-                <div className="flex justify-center items-center text-[14px] text-xl gap-[5px] p-[10px] bg-raisin-black rounded-[8px] text-white">
-                  <SupportImg color="white" />
-                  <span className="footer_support__gb0LW group-hover/support:text-cool-grey">
-                    Live chat
-                  </span>
-                </div>
-              </div>
             </div>
           </div>
-          <div className="footer_languageSwitcher__rUqv_ hidden mobile:flex flex-col gap-[0px] h-[80px]">
-            <div className="language_short__bH9Qg !justify-start !items-start group/active hover: cursor-pointer w-full h-full flex transition duration-300 text-xl gap-[5px] text-white relative">
-              <div className="z-10 !top-[25px] !left-0 !min-w-[40px] !w-[90px] language_bottom__mbDSA bg-dark-charcoal flex flex-col gap-[15px] p-[10px] undefined invisible">
-                <div className="group flex items-center gap-[10px] cursor-pointer">
-                  <Image
-                    src={ukIcon}
-                    className="w-[12px] h-[12px]"
-                    alt="logo"
-                  />
-                  <span className="group-hover:text-aztec-gold text-white transition duration-300 select-none text-sm">
-                    ENG
-                  </span>
-                </div>
-              </div>
-              <div className="flex items-center gap-[10px]">
-                <Image src={ukIcon} className="w-[12px] h-[12px]" alt="logo" />
-                <span className="text-white select-none text-sm">ENG</span>
-              </div>
-              <div className="language_shape__YoCxZ cursor-pointer">
-              <Image
-                alt="Arrow icon"
-                loading="lazy"
-                width={18}
-                height={18}
-                src="/icons/arrow-down.svg"
-                className="transition-all"
-              />
-              </div>
-            </div>
+          <div className="hidden max-[1000px]:flex flex-col gap-[0px] h-[80px]">
+            <a
+              href="mailto:support@reflow.im"
+              target="_blank"
+              className="h-fit w-fit mb-[15px] mt-[7px] select-none"
+            >
+              <div className="text-white text-lg">support@reflow.im</div>
+            </a>
             <div
-              className="group/support w-fit cursor-pointer hidden mobile:flex"
+              className="group/support w-fit cursor-pointer hidden max-[1000px]:flex"
               rel="noopener noreferrer"
             >
-              <div className="flex justify-center items-center text-[14px] text-xl gap-[5px] p-[10px] bg-raisin-black rounded-[8px] text-white">
+              <div className="flex justify-center items-center text-[14px] text-xl gap-[5px] p-[10px] bg-raisin-black rounded-[8px] text-white border border-[#5D5D5D]">
                 <SupportImg color="white" />
                 <span className="footer_support__gb0LW group-hover/support:text-cool-grey">
                   Live chat
                 </span>
               </div>
             </div>
-          </div>
-          <div className="mobile:flex hidden items-center gap-[10px] mt-[-40px]">
-            <div>
-              <div className="w-9 h-9 rounded-[18px] bg-gray-600 flex justify-center items-center text-white">
-                18+
-              </div>
-            </div>
-            <span className="text-xs leading-[14px] text-md">
-              By using this site, you confirm that you are 18 years old or
-              older.
-            </span>
           </div>
         </div>
       </div>
