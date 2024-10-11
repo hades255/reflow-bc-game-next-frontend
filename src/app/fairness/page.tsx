738f0eb1db79bfe02c9f6428c031db924c5363c9
bberry124 <renning100@gmail.com>
2024-10-11 23:23:41 +0900

    update

--- src/app/fairness/page.tsx
+++ src/app/fairness/page.tsx
@@ -38,7 +38,7 @@ const Fairness: FC = () => {
               : "border-[#212121B2] text-[#717171]"
           } w-[90px] h-[34px] bg-[#212121B2] rounded-[5px] border flex flex-row items-center justify-center text-[12px] font-semibold cursor-pointer`}
         >
-          Roulette
+          King's Roll
         </div>
         <div
           onClick={() => setTab(3)}
@@ -48,7 +48,7 @@ const Fairness: FC = () => {
               : "border-[#212121B2] text-[#717171]"
           } w-[90px] h-[34px] bg-[#212121B2] rounded-[5px] border flex flex-row items-center justify-center text-[12px] font-semibold cursor-pointer`}
         >
-          Royalflip
+          Royal Flip
         </div>
         <div
           onClick={() => setTab(4)}
